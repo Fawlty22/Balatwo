1ae52caefc0ad081215d894ac7118ba2e82b225f
Matt Keys <MatthewjKeys22@gmail.com>
2024-02-23 21:25:23 -0500

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'theme-blue': '#0A1D56',
+        'theme-purple': '#492E87',
+        'theme-green': '#37B5B6',
+        'theme-yellow': '#F2F597',
+        'theme-beige': '#F4E8D1',
+
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
