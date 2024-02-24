dfaeb2b5577023c1167d13b63cbc2401f1b2ebd5
Matt Keys <MatthewjKeys22@gmail.com>
2024-02-23 21:45:20 -0500

    basic card

---
+++ src/app/components/playing-card/playing-card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-playing-card',
+  standalone: true,
+  imports: [MatCardModule, MatIconModule],
+  templateUrl: './playing-card.component.html',
+  styleUrl: './playing-card.component.scss'
+})
+export class PlayingCardComponent {
+
+}
